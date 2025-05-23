00316afe8107e28fc7c43d880e6bf47ab19da35a
Davi - WSL <davi.frrs@outlook.com>
2025-05-23 09:22:34 -0300

    test: env

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -166,7 +166,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Ai Helper.</span>
+                <span className="text-base font-semibold">Ai Helper. Copy</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
